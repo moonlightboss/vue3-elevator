b60d97221593cdd7cb4144a9559bee99dc298314
moonlightboss <cumrak.legenda@gmail.com>
2023-09-18 13:12:00 +0300

    Add movement component and logic

---
+++ src/components/ElevatorMovement.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="elevator" :style="elevatorStyle"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentFloor: {
+      type: Number,
+      required: true
+    },
+    elevatorState: {
+      type: String,
+      required: true
+    },
+    targetFloor: {
+      type: Number
+    },
+    isMoving: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    elevatorStyle() {
+      return {
+        top: `${100 * (6 - this.currentFloor - 1)}px` // Вычисляем позицию лифта, что бы движение было сверху вниз.
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.elevator {
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  background-color: blueviolet;
+  transition: top 0.5s;
+}
+</style>
